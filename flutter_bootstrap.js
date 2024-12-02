eb456b4f58a24e8766813df7f44a627d90854810
bytarch <159407182+bytarch@users.noreply.github.com>
2024-12-01 22:31:42 -0600

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "70301618"
+    serviceWorkerVersion: "4200461880"
   }
 });
